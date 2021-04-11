3abd4046543f2c901f7485b057871a60796c46a4
Paweł Kocan <pkocan@onet.pl>
2021-04-11 13:39:09 +0200

    Removed unused classes

--- src/main/java/pl/edu/prz/master/thesis/backend/exception/ValidPeriodsException.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.prz.master.thesis.backend.exception;
-
-public class ValidPeriodsException extends RuntimeException {
-    public ValidPeriodsException() {
-        super("One or more of periods are wrong.");
-    }
-}
