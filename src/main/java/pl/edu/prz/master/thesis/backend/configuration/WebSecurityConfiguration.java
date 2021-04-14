a2c0798d198afbb78af07aeb9b94d8030c050032
Paweł Kocan <pkocan@onet.pl>
2021-04-14 18:00:59 +0200

    Quick fix

--- src/main/java/pl/edu/prz/master/thesis/backend/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/edu/prz/master/thesis/backend/configuration/WebSecurityConfiguration.java
@@ -66,7 +66,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/auth/**");
+        http.csrf().disable();
+//        http.csrf().ignoringAntMatchers("/auth/**");
         http.cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint()).and()
